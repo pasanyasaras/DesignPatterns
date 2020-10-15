35a859c1ee8e68631d40abefbc3331f698e0d7a6
pasanyasaras <pasansmart@gmail.com>
2020-10-15 19:58:39 +0530

    initial commit

---
+++ src/com/pasanyasara/Main.java
@@ -0,0 +1,11 @@
+package com.pasanyasara;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.name="Pasan";
+        System.out.println(user.name);
+    }
+}
---
+++ src/com/pasanyasara/User.java
@@ -0,0 +1,7 @@
+package com.pasanyasara;
+
+public class User {
+    public String name;
+
+    
+}
